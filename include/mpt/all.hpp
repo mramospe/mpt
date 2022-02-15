93571fc1dd62471a16bc7ab165cae1850ca27376
Miguel Ramos Pernas <miguel.ramos.pernas@gmail.com>
2022-02-15 13:45:06 +0000

    Add global header and corresponding test

---
+++ include/mpt/all.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "mpt/iterations.hpp"
+#include "mpt/keywords.hpp"
+#include "mpt/signature.hpp"
+#include "mpt/smart_enum.hpp"
+#include "mpt/types.hpp"
+#include "mpt/values.hpp"
+
+/// Basic functionalities
+namespace mpt {}
---
+++ test/cpp/test_all.cpp
@@ -0,0 +1,9 @@
+#include "mpt/all.hpp"
+
+using namespace mpt;
+using namespace mpt::keywords;
+using namespace mpt::smart_enum;
+using namespace mpt::signature;
+
+// Test that we can include all the headers without problems
+int main() { return 0; }
